fbf6e5dbec19038a78a377ba26d90113a26ff8dc
Lukey <ldipaola66@gmail.com>
2020-06-04 22:15:41 +1000

    added eventListner code for buttons

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
+var quizEl = document.querySelector("#quiz-main");
+var timer;
 var time = 30;
+var score = 0;
 timerEl.innerHTML = time;
+var questionIndex = 0;
+
+
+
 
 var questions =
       [
@@ -14,16 +21,30 @@ var questions =
         {
             question: "The external JavaScript file must contain the <script> tag.",
             choices: ["False", "True"],
-            answer: 1
+            answer: 0
         }
 
 
       ];
 
-      console.log(questions);
+      function renderQuestion() {
+       quizEl.textContent = "";
+       var newQuestion = document.createElement("p");
+       var newButton;
+        newQuestion.textContent = questions[questionIndex].question
+        quizEl.appendChild(newQuestion);
+        questions[questionIndex].choices.forEach(element => {
+            newButton = document.createElement('button');
+            newButton.setAttribute("data-number", questions[questionIndex].choices.indexOf(element));
+            newButton.textContent = element;
+            newButton.classList.add("quiz-buttons", "waves-effect", "waves-light", "btn", "grey", "darken-4", "button-style");
+            quizEl.appendChild(newButton);
+        });
+        
+      }
 
 function startTimer () {
-    var timer = setInterval(function(){
+        timer = setInterval(function(){
         if(time === 0){
             clearInterval(timer);
         }
@@ -38,5 +59,54 @@ function setTimer() {
     timerEl.innerHTML = time;
 
 }
+function startQuiz() {
+    renderQuestion();
+    
+}
+
+function endGame(){
+    clearInterval(timer);
+    if(score > 0){
+        score += time;
+    }
+    
+    quizEl.textContent = "Game Over, your score: " + score;
+    
+
+
+} 
+
 
 startBtn.addEventListener("click", startTimer);
+startBtn.addEventListener("click", startQuiz);
+quizEl.addEventListener("click", function(e){
+    
+    var buttonValue = parseInt(e.target.getAttribute("data-number"));
+    if(buttonValue === questions[questionIndex].answer){
+        alert("Correct");
+        score += 10;
+        if(questionIndex < questions.length -1) {
+            questionIndex++;
+            renderQuestion();
+
+        }else{
+            endGame();
+        }
+    }else if(e.target.tagName === "BUTTON"){
+        alert("Wrong!");
+        time -= 10;
+        if(questionIndex < questions.length -1){
+        questionIndex++;
+        renderQuestion();
+        }else{
+            endGame();
+        }
+    }
+});
+
+
+
+
+
+
+
